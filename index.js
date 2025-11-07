b43a2f774c83c75ecb3ed9b2ccbde8183134c98c
van <v.tugusow@yandex.ru>
2025-11-07 17:38:29 +0300

    brand-blockV0.2

---
+++ index.js
@@ -0,0 +1,14 @@
+new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    centerInsufficientSlides:true,
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    centeredSlides: true,
+
+
+
+});
