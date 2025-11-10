3ecc6a7fdaa9ce7cba4a89e7b295efbc4a6fe187
van <v.tugusow@yandex.ru>
2025-11-10 18:14:10 +0300

    brand-block_v0.6

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
+        bulletClass: 'swiper-pagination-bullet',
     },
-    centerInsufficientSlides:true,
     slidesPerView: 'auto',
-    spaceBetween: 10,
-    slidesPerGroup: 1,
-    centeredSlides: true,
+    spaceBetween: 16,
+    watchOverflow: true,
+    slidesOffsetBefore: 16,
+    slidesOffsetAfter: 16,
 
+
+    breakpoints: {
+        320:  { slidesPerView: 'auto' },
+        640:  { slidesPerView: 'auto' },
+        720:  { slidesPerView: 'auto' },
+        768:  { destroy() {
+            }},
+        1280: { slidesPerView: 'auto' },
+    }
 
 
 });
